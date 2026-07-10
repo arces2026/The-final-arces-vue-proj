293754db1f8836cdce032c082ff7e8798af97863
arces2026 <marco.fullstack2@gmail.com>
2026-07-10 17:57:12 +0200

    Added riepilogo ordine

---
+++ src/views/backupCartView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Cart from '@/components/Cart.vue'
+import { getProducts } from '@/services/productService';
+import { useCartStore } from '@/stores/cartStore';
+
+const cartStore = useCartStore();
+// const prodotti = await getProducts('https://deploy-django-backend.onrender.com/api/v1/scarpe')
+console.log({items: cartStore.items})
+console.log({totalItems: cartStore.totalItems})
+
+</script> 
+
+<template>
+    <Cart
+    :data="cartStore.items"
+    />
+    <h2>Totale</h2>
+    <p>€ {{ cartStore.totalPrice.toFixed(2) }}</p>
+    <button type="button" @click="cartStore.clearCart">Clear cart</button>
+</template>
